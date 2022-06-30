e857e0423da1638348db55fe21052c729c6c3266
aidanperez2000 <aidan@perezwitt.com>
2022-06-30 17:52:15 -0600

    Remove Buttons that Don't do Anything

--- meal_count/frontend/CreateButtons.js
+++ meal_count/frontend/CreateButtons.js
@@ -2,38 +2,12 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import printTable from './printTable';
-import toggleRows from './toggleRows';
 import exportCSV from './exportCSV';
 
 export default function CreateButtons({data}) {
 
   return (
     <p>
-      <div style={{textAlign: 'left', width: '48%', display: 'inline'}}>
-        <ButtonGroup aria-label="Toggle Views" size="sm">
-          <Button
-            variant="outline-secondary"
-            title="Show the next 10 days only."
-            className={"print-hide"}
-            onClick={() => {toggleRows('rowShow10')}}>
-            Next 10 Days
-          </Button>
-          <Button
-            variant="outline-secondary"
-            title="Show the next 30 days only."
-            className={"print-hide"}
-            onClick={() => {toggleRows('rowShow30')}}>
-            Next 30 Days
-          </Button>
-          <Button
-            variant="outline-secondary"
-            title="Show the full schedule."
-            className={"print-hide"}
-            onClick={() => {toggleRows('rowShowAll')}}>
-            Full Schedule
-          </Button>
-        </ButtonGroup>
-      </div>
       <div style={{textAlign: 'right', width: '48%', display: 'inline', float:'right'}}>
       <ButtonGroup aria-label="Data Options" size="sm">
           <Button
